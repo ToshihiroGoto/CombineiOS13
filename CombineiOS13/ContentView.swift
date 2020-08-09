ffdf7f6b3787294c7685d4cc5f9ef19a29ce647b
Toshihiro Goto <toshihiro.goto@ysl.co.jp>
2020-08-10 08:45:25 +0900

    Initial Commit

---
+++ CombineiOS13/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CombineiOS13
+//
+//  Created by Toshihiro Goto on 2020/08/10.
+//  Copyright © 2020 Toshihiro Goto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
